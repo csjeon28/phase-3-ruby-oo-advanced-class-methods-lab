fb6a2ae9b67dc2dd6991f2a02d56a3166cd180d6
Christine Jeon <csjeon28@gmail.com>
2021-07-11 16:50:19 -0400

    commit tree 6b10f7adc8b65bdcded37a8553134ef7270b17bc

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,59 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(string)
+    song = self.new
+    song.name = string
+    song
+  end
+
+  def self.create_by_name(string)
+    song = self.create
+    song.name = string
+    song
+  end
+
+  def self.find_by_name(string)
+    self.all.find {|song| song.name == string}
+  end
+
+  def self.find_or_create_by_name(string)
+    self.find_by_name(string) || self.create_by_name(string)
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    split_name = filename.split(" - ")
+    artist_name = split_name[0]
+    song_name = split_name[1].gsub(".mp3", "")
+
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    split_name = filename.split(" - ")
+    artist_name = split_name[0]
+    song_name = split_name[1].gsub(".mp3", "")
+
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
